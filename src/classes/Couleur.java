3def330a42eecf958f1de3a120f37b2a5578ebb2
juliangainar <gainar.ionut@gmail.com>
2023-03-31 09:27:45 +0200

    Added : Classe Couleur

---
+++ src/classes/Couleur.java
@@ -0,0 +1,36 @@
+package classes;
+
+
+/**
+ * Classe couleur gerant les proprietes d'une couleur
+ * @author Iulian GAINAR
+ *
+ */
+public class Couleur {
+	// parametres
+	private String nom;
+	private int r;
+	private int g;
+	private int b;
+	
+	// constructeurs
+	/**
+	 * Constructeur de la classe Couleur prenant 4 parametres
+	 * @author Iulian GAINAR
+	 * @param nom
+	 * @param r
+	 * @param g
+	 * @param b
+	 */
+	public Couleur(String nom, int r, int g, int b) {
+		this.nom = nom;
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+	// accesseurs
+	
+	// methodes
+	
+	
+}
